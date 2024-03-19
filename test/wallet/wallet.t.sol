bd9a201929512cc05b66b1d2be2e25c7429dc3b3
Tamar-Radin <126589896+Tamar-Radin@users.noreply.github.com>
2024-03-19 16:07:22 +0000

    1

--- test/wallet/wallet.t.sol
+++ test/wallet/wallet.t.sol
@@ -12,11 +12,21 @@ contract WalletTest is Test {
     // Everything I need to start my test
     function setUp() public {
         w = new Wallet();
+        console.log(address(this));
     }
 
     function testDeposit() public {
+
+        uint256 balanceBefore = address(w).balance;
+	    // Call the deposit function of the Wallet contract with 1 ether
+        (bool success, ) = address(w).call{value: 1 ether}("");
+        require(success, "Deposit failed");
+	    uint256 balanceAfter = address(w).balance;
+	    assertEq(balanceAfter - balanceBefore, 1 ether, "expect increase of 1 ether");
+        
     }
 
     function testWithdraw() public {
+        
     }
 }
