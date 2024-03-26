984eaf4ddd3df424483bed94a2a62f7300346833
Tamar-Radin <126589896+Tamar-Radin@users.noreply.github.com>
2024-03-26 18:29:31 +0200

    Update wallet.sol

--- src/wallet/wallet.sol
+++ src/wallet/wallet.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.0;
 
 contract Wallet {
     address payable private owner;
-    mapping (address => uint256) public hasGabaim;
+    mapping (address => uint256) public gabaim;
 
     // Constructor: Initializes contract with owner and predefined addresses
     constructor() {
         owner = payable(msg.sender);
         // Predefined addresses that can withdraw
-        hasGabaim[0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d] = 1;
-        hasGabaim[0x7a3b914a1f0bD991BAf826F4fE9a47Bb9880d25f] = 1;
-        hasGabaim[0x0638cF39b33D063c557AE2bC4B5D22a790Ac8Fe4] = 1;
+        gabaim[0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d] = 1;
+        gabaim[0x7a3b914a1f0bD991BAf826F4fE9a47Bb9880d25f] = 1;
+        gabaim[0x0638cF39b33D063c557AE2bC4B5D22a790Ac8Fe4] = 1;
     }
 
     // Fallback function: Allows the contract to receive Ether
@@ -20,14 +20,14 @@ contract Wallet {
     // Withdraw function: Allows owner and predefined addresses to withdraw Ether
     function withdraw(uint256 amount) external {
         require(
-            owner == msg.sender || hasGabaim[msg.sender] == 1,
+            owner == msg.sender || gabaim[msg.sender] == 1,
             "Only the owner and predefined addresses are allowed to withdraw Ether"
         );
         require(address(this).balance >= amount, "Insufficient balance");
         payable(msg.sender).transfer(amount);
     }
 
-        function update(address oldGabai, address newGabai) public onlyOwner{
+    function update(address oldGabai, address newGabai){
     
         require(owner == msg.sender, "Only the owner can update"); //only owner can update gabaaim
         require(gabaim[oldGabai]==1,"the old gabai is not exist"); // check if gabbai exist in the hash
