767eb10de6572177b31d19bf4eef9c4b3ae570e6
Tamar-Radin <r9098051@gmail.com>
2024-05-02 13:50:31 +0300

    123

---
+++ test/stakingT/newTest.t.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.15;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "@hack/stakingT/staking.sol";
+
+
+contract StakingTest is Test {
+    
+    uint constant WAD = 10**18;
+    StakingRewards staking;
+    ERC20 rt;
+    ERC20 st;
+    address user= vm.addr(123);
+    //newStake public s;
+
+    // Everything I need to start my test
+    function setUp() public {
+        s = new newStake();
+        st = new ERC20('Staking_Token');
+        rt=new ERC20('Reward_Token');
+        staking= new StakingRewards(address(st), address(rt));
+        rt.mint(address(staking), 100_00*WAD);
+        st.mint(user, 1000*wad);
+        staking.up
+        console.log(address(this));
+    }
+
+   
+
+
+
+
+    function testReward() public {
+        address randAdress= vm.addr(1111);
+        vm.startPrank(randAdress);
+        uint amount = 50;
+        vm.deal(randAdress, amount);
+        vm.stopPrank();
+        
+    }
+
+}
